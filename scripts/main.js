49bdbeebebc6fbacac608f844a4c558ccfe85319
devisvil-Al <blatov.a1996@gmail.com>
2024-07-20 23:02:31 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -18,10 +18,7 @@ const configSliderActions = {
 async function greethings(){
     window.Telegram.WebApp.expand() 
     const tg = window.Telegram.WebApp
-    const dataHash = 'query_id=AAFCPbI6AAAAAEI9sjpkKRH0&user=%7B%22id%22%3A984759618%2C%22first_name%22%3A%22%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22devisvil%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1721492698&hash=c5c877983e0d790d0868f83006cf0f6c3305e0009b67de94266b5529ae711a9f'
-    const user = {allows_write_to_pm :  true, first_name :  "Александр", id :  984759618, language_code :  "ru", last_name :  "", username :  "devisvil"}
-    console.log({dataHash: tg.initData, user: tg.initDataUnsafe.user});
-    const check = await api.checkUser({data: tg.initData || dataHash, user: tg.initDataUnsafe.user || user })
+    const check = await api.checkUser({data: tg.initData, user: tg.initDataUnsafe.user })
     if(check.ok){
         const res = await api.auth(window.Telegram.WebApp.initDataUnsafe.user.id)
         console.log(res);
