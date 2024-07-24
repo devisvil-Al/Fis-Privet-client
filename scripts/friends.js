2a3cd36123a0a4524d7c7885e7ed19e472f056bb
devisvil-Al <blatov.a1996@gmail.com>
2024-07-24 19:28:53 +0300

    Update friends.js

--- scripts/friends.js
+++ scripts/friends.js
@@ -13,8 +13,8 @@ async function init(user){
     const {friends} = await api.getFriends(user.friends)
     friends.forEach(renderFriends)
     addFriends.addEventListener('click', () => {
-        const messageText = "Join me on this awesome app! Click here: https://t.me/your_bot?start=YOUR_REFERRAL_ID";
-        const telegramShareUrl = `https://t.me/share/url?url=${encodeURIComponent(link)}`;
+        const messageText = `Join me on this awesome app! Click here: https://t.me/your_bot?start=${user.telegramId}`;
+        const telegramShareUrl = `https://t.me/share/url?url=${encodeURIComponent(messageText)}`;
         window.Telegram.WebApp.openTelegramLink(telegramShareUrl);
     })
 }
